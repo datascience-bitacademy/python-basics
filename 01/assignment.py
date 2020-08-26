7105e1a0738ac55c5ed7584fbe63028202634c4a
datascience-bitacademy <datascience.bitacademy@gmail.com>
2020-08-26 17:29:10 +0900

    변수 & 치환문 연습

---
+++ 01/assignment.py
@@ -0,0 +1,56 @@
+# 치환문
+
+a = 1
+b = a + 1
+print(a, b)
+
+# 변수 할당값 오류 (연산 방향 오류)
+# 1 + a = c
+
+# 세미콜론으로 치환문을 구분할 수 있다.
+e = 3.5; f = 5.3
+print(e, f)
+
+# 여러 개를 한번에 치환할 수 있다.
+e, f = 3.8, 8.3
+print(e, f)
+
+# 같은 값을 여러 변수에 할당할 수 있다.
+# x = 30
+# y = 30
+# z = 30
+x = y = z = 30
+print(x, y, z)
+
+
+# 동적 타이핑(실행 중에 변수의 타입을 결정한다)
+a = 1
+print(type(a))
+a = 'hello'
+print(type(a))
+a = 3.14
+print(type(a))
+a = True
+print(type(a))
+
+# 확장 치환문
+a = 10
+# a = a + 10
+a += 10
+print(a)
+a -= 10
+print(a)
+
+
+# swap
+x = 10
+y = 20
+print('===== swap =====')
+print(x, y)
+#
+#  코드를 짜서
+#
+print(x, y)
+
+
+
--- 01/variable.py
+++ 01/variable.py
@@ -1,4 +1,22 @@
-friend = 1
-a = 10
+import keyword
 
-print(friend, a)
+# 변수 이름은 문자, 숫자, _ 로 구성된다.
+friend = 1
+a = 1
+my_name = '안대혁'
+_yourName = '둘리'
+
+# 에러
+# 특수문자는 _ 만 가능
+# friend$ = 1
+# a! = 2
+# 숫자는 앞에 오면 안됨
+# 1abc = 10
+# keyword(예약어) 는 변수 이름으로 사용할 수 없음
+# def
+
+print(keyword.kwlist)
+
+# 한글이름 변수 사용하기
+가격1 = 2000
+print(가격1)
