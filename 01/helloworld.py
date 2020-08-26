cb69adcec2f465df4930db35693d919d92c94647
datascience-bitacademy <datascience.bitacademy@gmail.com>
2020-08-26 16:07:19 +0900

    들여쓰기 연습

--- 01/helloworld.py
+++ 01/helloworld.py
@@ -1,1 +1,26 @@
+# helloworld.py
+
+# 블록 시작(전역범위)
 print('hello world')
+a = 2
+if a > 1:    # 블록 시작(조건문)
+    print(a)
+    print('big')
+    # 블록 끝(조건문)
+
+
+def my_func(i):
+    print('블록 시작(함수정의)')
+    if i > 10:
+        print('블록 시작(조건문)')
+    print('big')
+    print('...')
+    print('블록 끝(함수정의)')
+
+
+# 함수호출
+my_func(20)
+
+
+
+
