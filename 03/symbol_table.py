3fcd7b069b40d587c66c248ad9408b56a5632d91
datascience-bitacademy <datascience.bitacademy@gmail.com>
2020-09-01 09:52:10 +0900

    Symbol Table #1

--- 03/symbol_table.py
+++ 03/symbol_table.py
@@ -6,14 +6,24 @@ g_s = '마이콜'
 def g_f():
     l_a = 2
     l_s = '둘리'
-    g_a = 2
+    a = 2
     print(locals())
 
 
+print('====== Global Symbol Table VS Local Symbol Table ======')
 print(globals())
 g_f()
 
 print(g_a)
 # error: local symbol table은 함수가 실행이 끝나면 사라진다.
 # print(l_a)
+
+print('====== Object Symbol Table ======')
+
+g_f.n = 'hello'
+g_f.i = 10
+print(g_f.__dict__)
+
+
+
 
